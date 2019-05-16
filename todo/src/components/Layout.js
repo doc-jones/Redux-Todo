874a203909c927a97f236525cb41fd0ee7a0f5b1
doc-jones <37349558+doc-jones@users.noreply.github.com>
2019-05-16 02:06:01 -0400

    Fix bugs in Layout, TodoList, TodoListItem

--- todo/src/components/Layout.js
+++ todo/src/components/Layout.js
@@ -4,9 +4,9 @@ import { AppBar, Toolbar, Typography, Paper } from "@material-ui/core";
 const Layout = memo(props => (
   <Paper
     elevation={0}
-    style={{ padding: 0, margin: 0, backgroundColor: "#fafafa" }}
+    style={{ padding: 0, margin: 20, backgroundColor: "#fafafa" }}
   >
-    <AppBar color="primary" position="static" style={{ height: 64 }}>
+    <AppBar color="primary" position="static" style={{ height: 100 }}>
       <Toolbar style={{ height: 64 }}>
         <Typography color="inherit">TODO with HOOKS</Typography>
       </Toolbar>
